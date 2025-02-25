601ec72a90852aa37916a0648c4ae5f8cb8f5fc9
saidev16 <boutouditsaid@gmail.com>
2025-02-25 22:40:18 +0000

    get baseurl from env

--- bus-pos-client/app/lib/api.ts
+++ bus-pos-client/app/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: apiUrl,
   headers: {
     "Content-Type": "application/json",
   },
